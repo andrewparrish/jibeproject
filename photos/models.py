dee9cc6a80123bc369796c34311ed6972fabb79f
Andrew <andrewlaptop@ubuntu.(none)>
2013-11-08 11:58:18 -0500

    Adding more photo stuff

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.core.files.storage import FileSystemStorage
 
 # Create your models here.
+class Photo(models.Model):
+	name = models.CharField(max_length=200)
+	picture = models.ImageField(upload_to="images/")
+
+	def __unicode__(self):
+		return self.name
+
