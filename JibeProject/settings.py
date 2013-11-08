e6eb3ceeebf5b672e2167411ec8189fa514c5266
Andrew <andrewlaptop@ubuntu.(none)>
2013-11-08 11:53:05 -0500

    Added photo app and fixed settings

--- JibeProject/settings.py
+++ JibeProject/settings.py
@@ -1,4 +1,12 @@
 # Django settings for JibeProject project.
+from os.path import dirname, abspath, join, pardir
+import os
+
+
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+SETTINGS_ROOT = dirname(abspath(__file__))
+PROJECT_ROOT = abspath(join(SETTINGS_ROOT, pardir))
+
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -11,8 +19,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': PROJECT_ROOT+ '/storage.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -108,6 +116,7 @@ ROOT_URLCONF = 'JibeProject.urls'
 WSGI_APPLICATION = 'JibeProject.wsgi.application'
 
 TEMPLATE_DIRS = (
+    PROJECT_ROOT+'/templates',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
