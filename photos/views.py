7f6fbf0686fd78b7abccf3f292762bcd09a4e156
Robert Brownstein <robert.brownstein@gmail.com>
2013-11-08 12:36:48 -0500

    revised logic in view

--- photos/views.py
+++ photos/views.py
@@ -4,13 +4,15 @@ from django.template import RequestContext
 from django.shortcuts import render
 from photos.models import Photo
 import flickr
+impott get_dctech
 
 def home(request):
 	tags = ["dctech"]
-    	photos = flickr.photos_search(tags=tags, per_page=40)
-	urls = []
+	sort = "interestingness-desc"
+	extras = "url_m,views"
+    	photos = get_dctech.photos_search_adv(tags=tags, per_page=40, sort=sort, extras=extras)
+	urls_and_views = []
 	for photo in photos:
-		urls.append(photo.getURL(size='Square', urlType='source'))
-	return render_to_response("photo/photos.html", dict(photos=urls))
-
+		urls_and_views.append(photo)
+	return render_to_response("photo/photos.html", dict(photos=urls_and_views))
 
